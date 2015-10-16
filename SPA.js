91266cc3a7fe09f3504a0211fe144e0b4f1c3c0f
fitzco01 <fitzco01@luther.edu>
2015-10-16 13:13:59 -0500

    Update SPA.js

--- SPA.js
+++ SPA.js
@@ -8,3 +8,6 @@ NewTask = function() {
   list.appendChild(item)
   item.appendChild(input)
 } 
+//Query Selector is an HTML element
+//By Monday:
+//Have code working so that new tasks can be added (completed as far as possible)
