491eedcb3fa608598b221ffc9e5e0954235fc01a
fitzco01 <fitzco01@luther.edu>
2015-10-19 13:09:35 -0500

    Update SPA.js

--- SPA.js
+++ SPA.js
@@ -1,14 +1,17 @@
 NewTask = function() {
-  input = document.getElementById("#text").value;
+  input = document.getElementById("#text");
+  inputtext = document.getElementById("#text").value;
   ul = document.getElementById("#mylist");
-  li = document.getElementById("#item")
+  item = document.createElement(li);
   
   document.body.appendChild(ul);
-  document.body.appendChild(input)
-  document.body.appendChild(li)
+  document.body.appendChild(inputtext);
+  document.body.appendChild(item);
+  document.body.appendChild(input);
   
-  ul.appendChild(li)
-  li.appendChild(input)
+  ul.appendChild(item);
+  item.appendChild(input);
+  item.appendChild(inputtext);
 } 
 //By Monday:
 //Have code working so that new tasks can be added
